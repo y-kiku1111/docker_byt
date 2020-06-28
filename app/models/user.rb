1e8f8e215cb3a13befc5ba7a765c744f9cfd2d44
y-kiku1111 <y.kikuchi.0925@gmail.com>
2020-06-29 00:38:52 +0900

    readmeの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,12 @@ class User < ApplicationRecord
   has_many :catch_copies
   has_many :likes
   has_many :likes_catch_copies, through: :likes
+
+
+  def self.guest
+    find_or_create_by!(nickname: 'ゲスト', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   resources :users, only: :show
   root to: 'catch_copies#index'
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
- 
-  post '/catch_copies/guest_sign_in', to: 'catch_copies#new_guest'  
-  
+   
   resources :catch_copies do
     resources :comments, only: :create
     collection do
