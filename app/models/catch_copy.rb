7cdd713ea59f1ed23437da2f4f5bbc508d9078d9
y-kiku1111 <y.kikuchi.0925@gmail.com>
2020-07-15 17:45:51 +0900

    バリデーションの修正

--- app/models/catch_copy.rb
+++ app/models/catch_copy.rb
@@ -1,10 +1,9 @@
 class CatchCopy < ApplicationRecord
+  validates :title,:image,           presence: true
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
   has_many :comments, dependent: :destroy
-
-
 
   def self.search(search)
     return CatchCopy.all unless search
