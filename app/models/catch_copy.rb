35c8f3be086a69b3138c4c19dac8320938fee48f
y-kiku1111 <y.kikuchi.0925@gmail.com>
2020-07-17 19:36:04 +0900

    微修正

--- app/models/catch_copy.rb
+++ app/models/catch_copy.rb
@@ -5,6 +5,7 @@ class CatchCopy < ApplicationRecord
   has_many :users, through: :likes
   has_many :comments, dependent: :destroy
 
+
   def self.search(search)
     return CatchCopy.all unless search
     CatchCopy.where('title LIKE(?)', "%#{search}%")
